1aacde95ff6bcaed6f81df5849ee5df048ca7adb
Pedro Felippe <pedfelippe@gmail.com>
2025-08-07 14:05:01 -0300

    Update .gitignore

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,6 +1,9 @@
 import { requireEnv } from '../utils/require-env'
 import dotenv from 'dotenv'
-dotenv.config()
+
+dotenv.config({
+  path: process.env.NODE_ENV === 'production' ? '.env.production' : '.env.local',
+})
 
 export const CLIENT_ID = requireEnv('SPOTIFY_CLIENT_ID')
 export const CLIENT_SECRET = requireEnv('SPOTIFY_CLIENT_SECRET')
